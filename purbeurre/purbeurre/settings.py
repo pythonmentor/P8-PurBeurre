bc63a4d546c7ff3b8d7f4372e3ac87c5fbb13a6e
itwasmee <wissam.shalby@gmail.com>
2020-03-19 00:50:30 +0100

    static dir

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/purbeurre/static/"
 
 LOGIN_URL = "login/"
 
