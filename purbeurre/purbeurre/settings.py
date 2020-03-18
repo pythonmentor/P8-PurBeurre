7247f4dc8ffcf338cee5a40937f9506f6b56aef7
itwasmee <wissam.shalby@gmail.com>
2020-03-18 21:55:36 +0100

    collectstatic 0

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -148,5 +148,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "purbeurre/static"),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 django_heroku.settings(locals())
