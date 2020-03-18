599b71593a7aadb4629bb3887097fa7b649f3df8
itwasmee <wissam.shalby@gmail.com>
2020-03-18 21:10:32 +0100

    deployment

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -148,7 +148,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'purbeurre/static'),
     )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
